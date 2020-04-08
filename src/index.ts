69d3c708e79bfe1470254b0719de5cb83285c116
KvisazGames <kiko@kiko.kiko>
2020-04-08 16:18:48 +0700

    master -  electron

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 const fs = require('fs');
 const path = require('path');
+const {BrowserWindow, app} = require('electron');
 
 console.log('Hello from main')
 const isExist = fs.existsSync(__filename);
+
+app.whenReady().then(createWindow)
+
+/***************
+ *  FUNCTIONS
+ *****************/
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    const appPath = path.join(app.getAppPath(), 'render/win_main/index.html');
+
+    win.loadFile(appPath);
+}
 
 
